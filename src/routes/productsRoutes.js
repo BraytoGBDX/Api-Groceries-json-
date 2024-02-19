b553c3060c403db6901d2611b4484bfe004400a5
Brayan Roberto Garcia Bernabe <146780823+BraytoGBDX@users.noreply.github.com>
2024-02-19 16:42:42 -0600

    Update productsRoutes.js

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { deleteProduct, getAllProducts, getOneProduct, insertProduct, loadUpdatePage, updateProduct } from "../controllers/productsController.js";
+import { deleteProduct, getAllProducts, getOneProduct, insertProduct, updateProduct } from "../controllers/productsController.js";
 const router=Router()
 
 router.get('/',getAllProducts)
 router.get('/:barcode',getOneProduct)
-router.post('/',insertProduct)
-router.post('/:barcode',updateProduct)
-router.get('/delete/:barcode',deleteProduct)
+router.post('/insert/',insertProduct)
+router.put('/update/:barcode',updateProduct)
+router.delete('/delete/:barcode',deleteProduct)
 
 
 
