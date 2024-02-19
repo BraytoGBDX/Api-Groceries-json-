fb1c1b4572d7c3cd7a3f54ee270fbb0f462121d0
BraytoGBDX <lculpa45@gmail.com>
2024-02-19 16:20:03 -0600

    giteando proyecto con rutas json

---
+++ src/controllers/productsController.js
@@ -0,0 +1,79 @@
+import producDAO from "../dao/productsDao.js";
+
+const C = console.log.bind(console.log)
+
+
+export const getAllProducts=(req,res)=>{
+    producDAO.getAll()
+    .then(products=>{
+        if(products!=null){
+            res.json(products)
+        }else{
+            res.json({
+                status:"Products not found"
+            })
+        }
+    })
+    .catch(err=>console.error(err))
+};
+
+export const getOneProduct=(req,res)=>{
+    producDAO.getOne(req.params.barcode)
+    .then(products=>{ 
+        if(products!=null){
+            res.json(products)
+        }else{
+            res.json({
+                status:"Product not found "
+            })
+        }
+    })
+    .catch(err=>res.json({
+        status:"Server unavailable"
+    }))
+}
+
+export const insertProduct=(req,res)=>{
+producDAO.insertProduct(req.body)
+.then(result=>{
+    if(result){
+        res.json(result)    
+    }
+})
+.catch(err=>{
+    res.json({
+        status:"Server unavailable"
+    })
+})
+}
+
+export const updateProduct=(req,res)=>{
+    producDAO.updateProduct(req.params.barcode,req.body)
+    .then(result=>{
+        if(result){
+            res.json(result)    
+        }
+    })
+    .catch(err=>{
+        res.json({
+            status:"Server unavailable"
+        })
+    })
+}
+
+export const deleteProduct=(req,res)=>{
+    producDAO.deleteProduct(req.params.barcode)
+    .then(result=>{
+        if(result){
+            res.json(result)    
+
+        }
+    })
+    .catch(err=>{
+        res.json({
+            status:"Server unavailable"
+        })
+    })
+}
+
+
